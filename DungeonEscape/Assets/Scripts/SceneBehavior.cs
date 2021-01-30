a3446754a186a099e4d455a515427cf593eb9b7c
Ather_Weather <marcelstaniszewski@gmx.de>
2021-01-30 20:05:27 +0100

    Start und EndScene

---
+++ DungeonEscape/Assets/Scripts/SceneBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneBehavior : MonoBehaviour
+{
+    public int levelIndex;
+    public bool curserLocked = false;
+    public bool curserVisible = true;
+
+    public void changeSceneTo(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void closeApplication()
+    {
+        Application.Quit();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelIndex);
+            setCurser();
+        }
+    }
+
+    void setCurser()
+    {
+        if (!curserLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        Cursor.visible = curserVisible;
+    }
+
+}
